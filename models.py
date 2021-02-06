c71c1449c3e6efe4e817c975b80a2a9012d5bf5c
jesravnbol <jra@kapacity.dk>
2021-02-06 23:23:20 +0100

    black.

--- models.py
+++ models.py
@@ -34,31 +34,25 @@ class NearEarthObject:
     initialized to an empty collection, but eventually populated in the
     `NEODatabase` constructor.
     """
-    # TODO: How can you, and should you, change the arguments to this constructor?
-    # If you make changes, be sure to update the comments in this file.
     def __init__(
-            self,
-            designation: str,
-            name: Optional[str] = None,
-            diameter: Optional[float] = None,
-            pha: Optional[bool]= None,
+        self,
+        designation: str,
+        name: Optional[str] = None,
+        diameter: Optional[float] = None,
+        hazardous: Optional[bool] = None,
     ):
         """Create a new `NearEarthObject`.
 
         :designation: primary designation of the asteroid or comet
         :name: IAU name
-        :param info: A dictionary of excess keyword arguments supplied to the constructor.
+        :diameter: Diameter of asteroid or meteor.
+        :hazardous: if the asteroid or meteor is hazardous
         """
-        # TODO: Assign information from the arguments passed to the constructor
-        # onto attributes named `designation`, `name`, `diameter`, and `hazardous`.
-        # You should coerce these values to their appropriate data type and
-        # handle any edge cases, such as a empty name being represented by `None`
-        # and a missing diameter being represented by `float('nan')`.
 
         self.designation = designation
         self.name = None if name == "" else str(name)
-        self.diameter = float(diameter) if diameter else float('nan')
-        self.hazardous = pha
+        self.diameter = float(diameter) if diameter else float("nan")
+        self.hazardous = hazardous
 
         # Create an empty initial collection of linked approaches.
         self.approaches = []
@@ -82,8 +76,10 @@ class NearEarthObject:
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""
-        return (f"NearEarthObject(designation={self.designation!r}, name={self.name!r}, "
-                f"diameter={self.diameter:.3f}, hazardous={self.hazardous!r})")
+        return (
+            f"NearEarthObject(designation={self.designation!r}, name={self.name!r}, "
+            f"diameter={self.diameter:.3f}, hazardous={self.hazardous!r})"
+        )
 
 
 class CloseApproach:
@@ -99,6 +95,7 @@ class CloseApproach:
     private attribute, but the referenced NEO is eventually replaced in the
     `NEODatabase` constructor.
     """
+
     # TODO: How can you, and should you, change the arguments to this constructor?
     # If you make changes, be sure to update the comments in this file.
     def __init__(self, **info):
@@ -110,7 +107,7 @@ class CloseApproach:
         # onto attributes named `_designation`, `time`, `distance`, and `velocity`.
         # You should coerce these values to their appropriate data type and handle any edge cases.
         # The `cd_to_datetime` function will be useful.
-        self._designation = ''
+        self._designation = ""
         self.time = None  # TODO: Use the cd_to_datetime function for this attribute.
         self.distance = 0.0
         self.velocity = 0.0
@@ -134,7 +131,7 @@ class CloseApproach:
         # TODO: Use this object's `.time` attribute and the `datetime_to_str` function to
         # build a formatted representation of the approach time.
         # TODO: Use self.designation and self.name to build a fullname for this object.
-        return ''
+        return ""
 
     def __str__(self):
         """Return `str(self)`."""
@@ -145,5 +142,7 @@ class CloseApproach:
 
     def __repr__(self):
         """Return `repr(self)`, a computer-readable string representation of this object."""
-        return (f"CloseApproach(time={self.time_str!r}, distance={self.distance:.2f}, "
-                f"velocity={self.velocity:.2f}, neo={self.neo!r})")
+        return (
+            f"CloseApproach(time={self.time_str!r}, distance={self.distance:.2f}, "
+            f"velocity={self.velocity:.2f}, neo={self.neo!r})"
+        )
